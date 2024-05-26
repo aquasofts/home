250fa9fb8dba108820bdb0d5aface1ba2c064efa
Aquasoft <aquasoft520@gmail.com>
2024-05-26 21:23:06 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -82,7 +82,7 @@ onMounted(() => {
   document.oncontextmenu = () => {
     ElMessage({
       message: "为了浏览体验，本站禁用右键",
-      grouping: true,
+      grouping: false,
       duration: 2000,
     });
     return false;
