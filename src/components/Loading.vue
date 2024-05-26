cdc0a94ec3a7863757387c635558778278b0aba1
aquasofts <aquasoft520@gmail.com>
2024-05-26 11:57:00 +0800

    Add files via upload

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -60,7 +60,7 @@ const siteName = import.meta.env.VITE_SITE_NAME;
         bottom: 5px;
         border-radius: 50%;
         border: 3px solid transparent;
-        border-top-color: #a4a4a4;
+        border-top-color: #d418ce;
         animation: spin-reverse 0.6s linear infinite;
       }
 
@@ -73,7 +73,7 @@ const siteName = import.meta.env.VITE_SITE_NAME;
         bottom: 15px;
         border-radius: 50%;
         border: 3px solid transparent;
-        border-top-color: #d3d3d3;
+        border-top-color: #d418ce;
         animation: spin 1s linear infinite;
       }
     }
